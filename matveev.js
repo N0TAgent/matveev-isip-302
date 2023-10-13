5905936f4795273f2b2e2b24d3b2b8737f67607e
N0TAgent <147793134+N0TAgent@users.noreply.github.com>
2023-10-13 10:21:40 +0500

    Add files via upload

---
+++ matveev.js
@@ -0,0 +1,66 @@
+// let num = -1
+// console.log(num);
+// if (num <0 ) {
+//     console.log("отрацательное число")
+// }
+// else {
+//     console.log("положительное число")
+// }
+
+
+
+
+// const str ="ооо брат тебе что чиф кифа включить иди сюда дорогой";
+// console.log(str.length);
+
+
+
+
+// let str = "чифкиф";
+// let h = str[str.length -1];
+// console.log(h);              
+
+
+
+
+
+// let n = 11
+// if (n % 2 === 0) {
+//     console.log("четное")
+// }
+// else {
+//     console.log("нечетное")
+// }
+
+
+
+
+
+// let word1 = 'мама'
+// let word2 = 'мапа'
+// if (word1[0] === word2[0]){
+//     console.log('Первые буквы совпадают');
+// } else {
+//     console.log('Первые буквы не совпадают');
+// }
+
+
+
+
+// let num = 12314
+// let fd = num.toString()[0];
+// let ld = num.toString().slice(-1);
+// let sum = +fd + +ld;
+// console.log('сумма первой и последней: ', sum);
+
+
+
+
+// let num = 123456;
+// let summa = 0;
+
+// while (num > 0){
+//     summa += num % 10;
+//     num = Math.floor(num / 10);
+// }
+// console.log('Сумма цифр: '+ summa);
